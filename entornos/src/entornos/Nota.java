b8c53a982cc022cac9c5242c8547e34a3ef6960a
Erik <Erik@DESKTOP-G9JUAQQ>
2019-05-05 21:53:48 +0200

    subo el segundo cambio

--- entornos/src/entornos/Nota.java
+++ entornos/src/entornos/Nota.java
@@ -6,9 +6,14 @@ public class Nota {
 		
 		System.out.println("hola colega a trabajar");
 		// aqui he bajado lo que habia subido ivan
+		System.out.println();
 		
 		System.out.println("hago el primero cambio en el proyecto y la subo ");
-
+        // aqui  he bajodo el cambio que hizo asier
+		System.out.println();
+		
+		System.out.println("hago el segundo cambio y la subo");
+		
 	}
 
 }
