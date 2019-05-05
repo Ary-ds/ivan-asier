33328b07eeac6d58c233db2c6be427bcaeada136
Erik <Erik@DESKTOP-G9JUAQQ>
2019-05-05 21:27:29 +0200

    subo el primero cambio

--- entornos/src/entornos/Nota.java
+++ entornos/src/entornos/Nota.java
@@ -5,6 +5,9 @@ public class Nota {
 	public static void main(String[] args) {
 		
 		System.out.println("hola colega a trabajar");
+		// aqui he bajado lo que habia subido ivan
+		
+		System.out.println("hago el primero cambio en el proyecto y la subo ");
 
 	}
 
